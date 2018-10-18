b81fb9a934fa3dd19060c28091b3e28fefa0295c
Raayan <Raayan@DESKTOP-5FM3PCD>
2018-10-18 17:06:40 +0530

    array objects and value output

---
+++ src/main/java/com/test/javaPractice/TestMain.java
@@ -0,0 +1,18 @@
+package com.test.javaPractice;
+
+public class TestMain {
+
+	public static void main(String[] args) 
+	{
+		//learning arrayDeclaration & printing value
+		CommonUtility ac=new CommonUtility();
+		ac.arrayDeclaration();
+		
+		System.out.println();
+		System.out.println("2nd method result:");
+		ac.getArrayObj();
+		
+
+	}
+
+}
